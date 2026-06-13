38c59b7cbb0fc96beff4aef18028e6d377a5989b
Ikhsan Dimastianto <idimastianto@student.umrah.ac.id>
2026-06-13 20:09:01 +0700

    fix: change pgsql prepared statement setting for Supabase pooler compatibility

--- config/database.php
+++ config/database.php
@@ -97,9 +97,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
-            'options' => [
-                \PDO::ATTR_EMULATE_PREPARES => true,
-            ],
+            'prepared' => false,
         ],
 
         'sqlsrv' => [
